b009fa5423582ab8494230e91c4563520e8565e2
Victor Malukhin <victor.malukhin@gmail.com>
2023-04-18 22:13:53 +0300

    Начальная настройка проекта

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+# Changelog
+All notable changes to this project will be documented in this file.
+
+Все заметные изменения в этом проекте будут задокументированы в этом файле.
+
+## [0.0.1] - 18-04-2023
+### Добавлено
+- Проект создан
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  constraints subdomain: 'next' do
+    # root 'counterparties#index'
+  end
 end
